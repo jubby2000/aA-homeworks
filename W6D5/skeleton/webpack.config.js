90a4c25bf3bcf5581ccad4d74e6f4c7141c96d0b
jacob barlow <jubby2000@gmail.com>
2018-05-26 21:38:02 -0700

    Create store and actions with Redux

---
+++ W6D5/skeleton/webpack.config.js
@@ -0,0 +1,26 @@
+let path = require('path');
+
+module.exports = {
+  entry: './app.jsx',
+  output: {
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['env', 'react', 'es2015']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
